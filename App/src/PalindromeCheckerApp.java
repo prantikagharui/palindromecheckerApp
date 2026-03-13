1473c75de367cf626c8a8f6ed3f5ce4d90bf461d
prantikagharui <prantikagharui@gmail.com>
2026-03-13 09:35:46 +0530

    prantikagharui added UC3-PalindromeCheckUsingStringReverse

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,27 +2,24 @@
      * @author Prantika Gharui
      * @regno  RA2411056010199
      * @version 1.0
-     */
+     /***/
     import java.util.Scanner;
      public class PalindromeCheckerApp {
-        public static void main(String[] args) {
+         public static void main(String[] args) {
 
-            System.out.println("Welcome to the Palindrome Checker System");
-            System.out.println("Version : 1.0");
-            System.out.println("System initialized successfully.");
-
-            //UC2
-            Scanner sc= new Scanner(System.in);
-            System.out.print("Enter Name:");
-            String input=sc.nextLine();
-            boolean isPalindrome = true;
-            for(int i=0; i< input.length()/2; i++) {
-                if(input.charAt(i) != input.charAt(input.length()-1 -i)){
-                    isPalindrome = false;
-                    break;
-                }
-            }
-        System.out.println("Is It a Palindrome? :" + isPalindrome);
+             Scanner sc = new Scanner(System.in);
+             System.out.print("Enter string:");
+             String input = sc.nextLine();
+             String reversed = "";
+             for (int i = input.length() - 1; i >= 0; i--) {
+                 reversed = reversed + input.charAt(i);
+             }
+            if(input.equals(reversed)) {
+             System.out.println("The string is a Palindrome");
+         } else {
+             System.out.println("The string is NOT a Palindrome");
+         }
             sc.close();
-        }
-    }
+         }
+     }
+
